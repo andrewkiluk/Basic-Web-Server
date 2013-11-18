d259de4f4e2e0a531d39ee73159cca52d61166e4
Andrew Kiluk <aek2140@columbia.edu>
2013-11-18 14:23:07 -0500

    Added dynamic content, cleaned up old directory

--- handle-http-client.c
+++ handle-http-client.c
@@ -11,11 +11,24 @@
 
 const int BUF_SIZE = 4096;
 
-void HandleHTTPClient(char *web_root, char *mdb_lookup_host, int mdb_lookup_port, int clntSock){
+void HandleHTTPClient(char *web_root, int clntSock, int mdbSock){
   char buffer[BUF_SIZE];
   char first_line[BUF_SIZE];
   char initial_request[BUF_SIZE];
   FILE *pipe = fdopen(clntSock,"r+");
+  if (pipe == NULL)
+    printf("Error opening clntSock as a file\n");
+  FILE *mdbpipe = fdopen(mdbSock,"r+");
+  if (mdbpipe == NULL)
+    printf("Error opening mdbSock as a file\n");
+  const char *mdb_lookup_form =
+  "<h1>mdb-lookup</h1>\n"
+  "<p>\n"
+  "<form method=GET action=/mdb-lookup>\n"
+  "lookup: <input type=text name=key>\n"
+  "<input type=submit>\n"
+  "</form>\n"
+  "<p>\n";
 
   if(fgets(buffer, BUF_SIZE - 1 , pipe) != NULL){
     
@@ -45,7 +58,7 @@ void HandleHTTPClient(char *web_root, char *mdb_lookup_host, int mdb_lookup_port
       // Now we check that none of these pointers are NULL.
       if ( !( method && requestURI && httpVersion) ) {
 	printf("\"%s\" 400 Bad Request\n", initial_request);
-	fprintf(pipe, "HTTP/1.0 400 Bad Request\r\n\r\n<html><body>\r\n<h1>400 Bad Request</h1>\r\n</body></html>\r\n\r\n");
+	fprintf(pipe, "HTTP/1.0 400 Bad Request\r\n\r\n<html><body>\r\n<h1>400 Bad Request</h1>\r\n</body></html>\r\n");
 	fflush(pipe);
 	close(clntSock);
 	return;
@@ -54,7 +67,7 @@ void HandleHTTPClient(char *web_root, char *mdb_lookup_host, int mdb_lookup_port
       // This server only supports GET requests, so we throw out anything else.
       if ( (strlen(method) < 3) || (strncmp("GET", method, 4) != 0) ) {
 	printf("\"%s %s %s\" 501 Not Implemented\n", method, requestURI, httpVersion);
-	fprintf(pipe, "HTTP/1.0 501 Not Implemented\r\n\r\n<html><body>\r\n<h1>501 Not Implemented</h1>\r\n</body></html>\r\n\r\n");
+	fprintf(pipe, "HTTP/1.0 501 Not Implemented\r\n\r\n<html><body>\r\n<h1>501 Not Implemented</h1>\r\n</body></html>\r\n");
 	fflush(pipe);
 	close(clntSock);
 	return;
@@ -62,7 +75,7 @@ void HandleHTTPClient(char *web_root, char *mdb_lookup_host, int mdb_lookup_port
       // We check that the request line is terminated by a valid HTTP version.
       if ((strlen(httpVersion) < 8) || ((strncmp("HTTP/1.0", httpVersion, 8) != 0) && (strncmp("HTTP/1.1", httpVersion, 8)) != 0) ) {
 	printf("\"%s %s %s\" 400 Bad Request\n", method, requestURI, httpVersion);
-	fprintf(pipe, "HTTP/1.0 400 Bad Request\r\n\r\n<html><body>\r\n<h1>501 Not Implemented</h1>\r\n</body></html>\r\n\r\n");
+	fprintf(pipe, "HTTP/1.0 400 Bad Request\r\n\r\n<html><body>\r\n<h1>501 Not Implemented</h1>\r\n</body></html>\r\n");
 	fflush(pipe);
 	close(clntSock);
 	return;
@@ -71,75 +84,142 @@ void HandleHTTPClient(char *web_root, char *mdb_lookup_host, int mdb_lookup_port
       // no references to parent directories for security reasons.
       if ((strncmp("/", requestURI, 1) != 0 ) || (strstr(requestURI, "..")) ) {
 	printf("\"%s %s %s\" 400 Bad Request\n", method, requestURI, httpVersion);
-	fprintf(pipe, "HTTP/1.0 400 Bad Request\r\n\r\n<html><body>\r\n<h1>400 Bad Request</h1>\r\n</body></html>\r\n\r\n");
+	fprintf(pipe, "HTTP/1.0 400 Bad Request\r\n\r\n<html><body>\r\n<h1>400 Bad Request</h1>\r\n</body></html>\r\n");
 	fflush(pipe);
 	close(clntSock);
 	return;
       }
-      // If the request has passed all of our tests, we should be ready to provide the file to
-      // the client. 
-      else{
-	// We first concatenate the server and user file paths.
-	char file_path[300];
-	strcpy(file_path, web_root);
-	strcat(file_path, requestURI);
+      // If the request has passed all of our tests, we should be ready to provide what the
+      // client requested.
 
-	// Now we need to check if the file requested is a directory. If so, we append index.html.
-	struct stat statinfo;
-	stat(file_path, &statinfo);
-	if (S_ISDIR(statinfo.st_mode)){
-	  strcat(file_path, "index.html");
-	}
-	FILE *reqFile;
+      // We first check if they have requested mdb-lookup.
+      if ( (strncmp(requestURI, "/mdb-lookup", 12) == 0) 
+	|| (strncmp(requestURI, "/mdb-lookup/", 12) == 0) ){
 	
-	// Here we actually open the file, sending a 404 if we can't do that.
-	if ((reqFile = fopen(file_path, "rb"))){
-	  printf("\"%s %s %s\" 200 OK\n", method, requestURI, httpVersion);
-	  fprintf(pipe, "HTTP/1.0 200 OK\r\n");
-	}
-	else{
-	  printf("\"%s %s %s\" 404 Not Found\n", method, requestURI, httpVersion);
-	  fprintf(pipe, "HTTP/1.0 404 Not Found\r\n\r\n<html><body>\r\n<h1>404 Not Found</h1>\r\n</body></html>\r\n\r\n");        
+	// Looks like an initial mdb-lookup request. We send a header saying that's cool, 
+	// and we log the event on the server.
+
+	fprintf(pipe, "HTTP/1.0 200 OK\r\n");
+	fprintf(pipe, "\r\n");
+	printf("\"%s %s %s\" 200 OK\n", method, requestURI, httpVersion);
+
+	// Next we send them the form to do mdb-lookup requests.
+	fprintf(pipe, "<html><body>\n%s</html></body>", mdb_lookup_form);
+	fflush(pipe);
+	close(clntSock);
+	return;
+      }
+      if ( strncmp(requestURI, "/mdb-lookup?key=", 16) == 0) {
+
+	// Ok, looks like they're submitting an mdb-lookup query. Let's point to the 
+	// actual query:
+
+	char *mdb_query = strchr(requestURI, '=') + 1;
+	
+	// We now send a header saying that's cool, and log the event on the server.
+
+	fprintf(pipe, "HTTP/1.0 200 OK\r\n");
+	fprintf(pipe, "\r\n");
+	
+	printf(" -> looking up [%s] \"%s %s %s\" 200 OK\n", mdb_query, method, requestURI, httpVersion);
+
+        // Now we send the query to mdb-lookup-server.
+	fprintf(mdbpipe, "%s\n", mdb_query);
+
+	// We are now ready to return the results to the client. We first send the beginning:
+
+	fprintf(pipe, "<html><body>\n%s\n", mdb_lookup_form);
+	fprintf(pipe, "<p><table border>\n");
+
+	// Now we recieve one row at a time from mdb-lookup-server, and send it to the client.
+        
+	int row_number = 0;
+	while( (fgets(buffer, BUF_SIZE - 1, mdbpipe) != NULL) 
+	    && (strcmp(buffer, "\n") != 0 ) ){
+	  if(row_number % 3 == 0){
+	    fprintf(pipe, "<tr><td bgcolor=C4D8E2>  %s", buffer);
+	  }
+	  if(row_number % 3 == 1){
+	    fprintf(pipe, "<tr><td>  %s", buffer);
+	  }
+	  if(row_number % 3 == 2){
+	    fprintf(pipe, "<tr><td bgcolor=DAA520>  %s", buffer);
+	  }
+          row_number++;
+        }
+
+	// Now we send the end of the HTML document, flush the output pipe, and 
+	// close the connection.
+
+	fprintf(pipe, "</table>\n</body></html>");
+	fflush(pipe);
+	close(clntSock);
+	return;
+      }
+
+      // At this point we know the user didn't request mdb-lookup services, so we next assume
+      // they want to download a file.
+
+      // We first concatenate the server and user file paths.
+      char file_path[300];
+      strcpy(file_path, web_root);
+      strcat(file_path, requestURI);
+
+      // Now we need to check if the file requested is a directory. If so, we append index.html.
+      struct stat statinfo;
+      stat(file_path, &statinfo);
+      if (S_ISDIR(statinfo.st_mode)){
+	strcat(file_path, "index.html");
+      }
+      FILE *reqFile;
+      
+      // Here we actually open the file, sending a 404 if we can't do that.
+      if ((reqFile = fopen(file_path, "rb"))){
+	printf("\"%s %s %s\" 200 OK\n", method, requestURI, httpVersion);
+	fprintf(pipe, "HTTP/1.0 200 OK\r\n");
+      }
+      else{
+	printf("\"%s %s %s\" 404 Not Found\n", method, requestURI, httpVersion);
+	fprintf(pipe, "HTTP/1.0 404 Not Found\r\n\r\n<html><body>\r\n<h1>404 Not Found</h1>\r\n</body></html>\r\n\r\n");        
+	fflush(pipe);
+	close(clntSock);
+	return;
+      }
+      
+      // Here we add some headers.
+      if(strstr(file_path, ".html")){
+	fprintf(pipe, "Content-Type: text/html\r\n");
+      }
+
+      if(strstr(file_path, ".jpg")){
+	fprintf(pipe, "Content-Type: image/jpg\r\n");
+      }
+
+      fseek(reqFile, 0, SEEK_END);
+      long fileSize = ftell(reqFile);
+      fseek(reqFile, 0, SEEK_SET);
+      fprintf(pipe, "Content-Length: %ld\r\n", fileSize);
+
+      fprintf(pipe, "\r\n");
+
+      // This loop writes the requested file into the pipe.
+      int bytes_read = 0;
+      while((bytes_read = fread(buffer, 1, BUF_SIZE, reqFile))){
+	if(bytes_read < 0){
+	  printf("Error in file access\n");
+	  fprintf(pipe, "HTTP/1.0 500 Internal Server Error\r\n\r\n<html><body>\r\n<h1>500 Internal Server Error</h1>\r\n</body></html>\r\n\r\n");
 	  fflush(pipe);
 	  close(clntSock);
 	  return;
 	}
-	
-	// Here we add some headers.
-	if(strstr(file_path, ".html")){
-	  fprintf(pipe, "Content-Type: text/html\r\n");
+	if(fwrite(buffer, 1, bytes_read, pipe) < 0){
+	  break;
 	}
-
-	if(strstr(file_path, ".jpg")){
-	  fprintf(pipe, "Content-Type: image/jpg\r\n");
-	}
-
-	fseek(reqFile, 0, SEEK_END);
-	long fileSize = ftell(reqFile);
-	fseek(reqFile, 0, SEEK_SET);
-	fprintf(pipe, "Content-Length: %ld\r\n", fileSize);
-
-	fprintf(pipe, "\r\n");
-
-	// This loop writes the requested file into the pipe.
-	int bytes_read = 0;
-	while((bytes_read = fread(buffer, 1, BUF_SIZE, reqFile))){
-	  if(bytes_read < 0){
-	    printf("Error in file access\n");
-	    fprintf(pipe, "HTTP/1.0 500 Internal Server Error\r\n\r\n<html><body>\r\n<h1>500 Internal Server Error</h1>\r\n</body></html>\r\n\r\n");
-	    fflush(pipe);
-	    close(clntSock);
-	    return;
-	  }
-	  if(fwrite(buffer, 1, bytes_read, pipe) < 0){
-	    break;
-	  }
-	}
-	
-	fflush(pipe);
-	close(clntSock);
-	return;
       }
+      
+      fflush(pipe);
+      close(clntSock);
+      return;
     }
   }
 }
--- http-server.c
+++ http-server.c
@@ -5,10 +5,11 @@
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
 #include <signal.h>
+#include <netdb.h>
 
 #define MAXPENDING 5    /* Maximum outstanding connection requests */
 
-void HandleHTTPClient(char *web_root, char *mdb_lookup_host, int mdb_lookup_port, int clntSock);   /* TCP client handling function */
+void HandleHTTPClient(char *web_root, int clntSock, int mdbSock);   /* TCP client handling function */
 
 static void DieWithError(const char *message)
 {
@@ -22,11 +23,14 @@ static void DieWithError(const char *message)
 int main(int argc, char *argv[])
 {
     int servSock;                    /* Socket descriptor for server */
+    int mdbSock;                     /* Socket descriptor for mdb-lookup-server */
     int clntSock;                    /* Socket descriptor for client */
     struct sockaddr_in ServAddr;     /* Local address */
     struct sockaddr_in ClntAddr;     /* Client address */
+    struct sockaddr_in mdbAddr;      /* mdb-lookup-server address */
     unsigned short ServPort;         /* Server port */
     unsigned int clntLen;            /* Length of client address data structure */
+    char *mdbIP;                     /* mdb-lookup-server IP address */
 
     
     // ignore SIGPIPE so that we don’t terminate when we call
@@ -40,8 +44,44 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage:  %s <server-port> <web_root> <mdb-lookup-host> <mdb-lookup-port>\n", argv[0]);
         exit(1);
     }
-
+    
     ServPort = atoi(argv[1]);  /* First arg:  local port */
+    char *webRoot = argv[2];
+    char *mdbHostname = argv[3];
+    unsigned short mdbPort = atoi(argv[4]);
+    
+
+
+    // We first want to establish a connection with the mdb-lookup-server.
+
+    
+    // Get mdb IP from server name
+    struct hostent *he;
+    if ((he = gethostbyname(mdbHostname)) == NULL) {
+      DieWithError("gethostbyname failed");
+    }
+    mdbIP = inet_ntoa(*(struct in_addr *)he->h_addr);
+//    free(he);
+
+    /* Create socket for mdb-lookup-server connection */
+    if ((mdbSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+        DieWithError("socket() failed");
+
+    /* Construct mdb-lookup-server address structure */
+    memset(&mdbAddr, 0, sizeof(mdbAddr));   /* Zero out structure */
+    mdbAddr.sin_family = AF_INET;           /* Internet address family */
+    inet_aton(mdbIP, &(mdbAddr.sin_addr));  /* mdb-lookup-server IP address */
+    mdbAddr.sin_port = htons(mdbPort);      /* mdb-lookup-server port */
+
+    /* Establish the connection to mdb-lookup-server */
+    if (connect(mdbSock, (struct sockaddr *) &mdbAddr, sizeof(mdbAddr)) < 0)
+      DieWithError("connect() failed");
+
+
+
+    // Now we are ready to establish a socket connection to listen for clients.
+
+
 
     /* Create socket for incoming connections */
     if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
@@ -63,21 +103,21 @@ int main(int argc, char *argv[])
 
     for (;;) /* Run forever */
     {
-        /* Set the size of the in-out parameter */
-        clntLen = sizeof(ClntAddr);
+      /* Set the size of the in-out parameter */
+      clntLen = sizeof(ClntAddr);
 
-        /* Wait for a client to connect */
-        if ((clntSock = accept(servSock, (struct sockaddr *) &ClntAddr, 
-                               &clntLen)) < 0){
-	  fprintf(stderr,"failed accept() attempt\n");
-	  break;
-	}
+      /* Wait for a client to connect */
+      if ((clntSock = accept(servSock, (struct sockaddr *) &ClntAddr, 
+                             &clntLen)) < 0){
+        fprintf(stderr,"failed accept() attempt\n");
+        break;
+      }
 
-        /* clntSock is connected to a client! */
+      /* clntSock is connected to a client! */
 
-        printf("%s ", inet_ntoa(ClntAddr.sin_addr));
+      printf("%s ", inet_ntoa(ClntAddr.sin_addr));
 
-      HandleHTTPClient(argv[2], argv[3], atoi(argv[4]), clntSock);
+      HandleHTTPClient(webRoot, clntSock, mdbSock);
     }
     /* NOT REACHED */
     return 0;
